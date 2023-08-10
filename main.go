1474e2ae37f478f80c8089472fe13c8691069d42
Christian Grøntved Andersen <christian.andersen@bestseller.com>
2023-08-10 19:16:12 +0200

    getting info into three seperate structs

--- main.go
+++ main.go
@@ -9,11 +9,15 @@ import (
 )
 
 // initializing a data structure to keep the scraped data
-type Product struct {
-	URL        string
-	Name       string
-	Attributes map[string]string
-	Variants   map[string]string
+type ProductDetails struct {
+	URL      string
+	Variants map[string]string
+}
+
+type ProductAttribute struct {
+	URL       string
+	Name      string
+	Attribute map[string]string
 }
 
 type Hierachy struct {
@@ -24,7 +28,8 @@ type Hierachy struct {
 func main() {
 
 	// initializing the slice of structs to store the data to scrape
-	var products []Product
+	var products []ProductDetails
+	var attributes []ProductAttribute
 	var hierarchies []Hierachy
 
 	c := colly.NewCollector(colly.AllowedDomains("www.hydroscand.dk"))
@@ -32,25 +37,28 @@ func main() {
 	// Create another collector to scrape product details
 	detailCollector := c.Clone()
 
-	// scraping logic
+	// Callback to find the links to the products on the page
 	c.OnHTML("li.sub-categories__item", func(e *colly.HTMLElement) {
 		link := e.ChildAttr("a", "href")
 		e.Request.Visit(link)
 	})
 
+	// Callback to find the links to the details page of each product
+	// and fetch the category hierarchy of the product
 	c.OnHTML("div.page-wrapper", func(e *colly.HTMLElement) {
 
 		hierachy := Hierachy{}
 		hierachy.Category = make(map[string]string)
 		levels := 0
 
+		// Extract the category hierarchy of the product
 		e.ForEach("div.breadcrumbs li[class*=cat]", func(_ int, el *colly.HTMLElement) {
 
 			hierachy.Category[fmt.Sprintf("Category_%v", levels)] = strings.TrimSpace(el.Text)
 			levels++
 
 		})
-
+		// Extract the links to the details page of each product
 		c.OnHTML("a.product-item-link", func(e *colly.HTMLElement) {
 
 			detailsLink := e.Attr("href")
@@ -62,33 +70,38 @@ func main() {
 
 	})
 
+	// Callback to extract the details of the product
 	detailCollector.OnHTML("div.product-info-wrapper", func(e *colly.HTMLElement) {
 		log.Println("Product description found", e.Request.URL)
 
-		product := Product{URL: e.Request.URL.String()}
-		product.Name = e.ChildText("h1.page-title > span.base")
-		product.Attributes = make(map[string]string)
+		productAttributes := ProductAttribute{URL: e.Request.URL.String()}
+		productAttributes.Name = e.ChildText("h1.page-title > span.base")
+		productAttributes.Attribute = make(map[string]string)
+
+		product := ProductDetails{URL: e.Request.URL.String()}
 		product.Variants = make(map[string]string)
 
-		// Extract the details of the product
+		// Extract the common attributes of the product
 		e.ForEach("table.data.table.additional-attributes:not(#product-attribute-specs-table) tr", func(_ int, el *colly.HTMLElement) {
 			key := el.ChildText("th")
 			val := el.ChildText("td")
 
-			product.Attributes[key] = val
+			productAttributes.Attribute[key] = val
 
 		})
 
+		// Extract the variants of the product
 		e.ForEach("div.product-variants table.table td", func(_ int, el *colly.HTMLElement) {
 			key := el.Attr("data-th")
 			value := strings.TrimSpace(el.Text)
 			product.Variants[key] = value
 		})
 
+		attributes = append(attributes, productAttributes)
 		products = append(products, product)
 
 	})
-	c.Visit("https://www.hydroscand.dk/dk_dk/produkter/adaptere/bsp-koblinger-og-adaptere")
+	c.Visit("https://www.hydroscand.dk/dk_dk/produkter")
 
 	for _, h := range hierarchies {
 		fmt.Printf("Hierachy: %+v\n", h)
