d859c4e525d3c7f0edac241685c364b9f6805a91
Christian Grøntved Andersen <christian.andersen@bestseller.com>
2023-08-10 23:07:07 +0200

    Fuck it, YOLO!

--- main.go
+++ main.go
@@ -9,34 +9,20 @@ func main() {
 
 	categories, attributes, products := scrape()
 
-	jsonBytes, err := json.Marshal(categories)
+	saveJson("categories.json", categories)
+	saveJson("attributes.json", attributes)
+	saveJson("products.json", products)
+
+}
+
+func saveJson(filename string, v any) {
+	jsonBytes, err := json.Marshal(v)
 	if err != nil {
 		panic(err)
 	}
 
-	err = os.WriteFile("categories.json", jsonBytes, 0644)
+	err = os.WriteFile(filename, jsonBytes, 0644)
 	if err != nil {
 		panic(err)
 	}
-
-	jsonBytes, err = json.Marshal(attributes)
-	if err != nil {
-		panic(err)
-	}
-
-	err = os.WriteFile("attributes.json", jsonBytes, 0644)
-	if err != nil {
-		panic(err)
-	}
-
-	jsonBytes, err = json.Marshal(products)
-	if err != nil {
-		panic(err)
-	}
-
-	err = os.WriteFile("products.json", jsonBytes, 0644)
-	if err != nil {
-		panic(err)
-	}
-
 }
